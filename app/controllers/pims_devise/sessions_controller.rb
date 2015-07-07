bac4d22816b6228dbe026b674cb3688ca0d1fa3e
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-07-08 01:27:32 +0530

    session creation after logging in

--- app/controllers/pims_devise/sessions_controller.rb
+++ app/controllers/pims_devise/sessions_controller.rb
@@ -1,55 +1,59 @@
 class PimsDevise::SessionsController < Devise::SessionsController
-	layout "login"
+  layout "login"
 
 
- def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
- def create
-		super
-	end
+  def create
+    super
+  end
 
-	def destroy
-		#super
+  def destroy
+    #super
     sign_out(User)
     @user = User.new
     redirect_to new_user_session_path
-	end
-
-
+  end
 
 
   private
 
   def sign_in_params
-		params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password)
   end
 
- protected
+  protected
 
 
+  def after_sign_in_path_for(user)
+    logger.debug "========after signin==========="
+    if me.valid_duration >= Time.now && me.valid_password?(params[:user][:password]) == me.valid_password?('password')
+      logger.debug "========First time user========"
+      #store current user in session
+      session[:user] = me.id
+      password_edit_user_path(me.id)
+    elsif me.valid_duration < Time.now
+      logger.debug "====Validity expired======="
+      sign_out(@user)
+      #redirect_to destroy_user_session_path, flash[:notice]= 'Your Validaty Period Has Expired!,Speak to Admin'
+    else
+      me.valid_duration >= Time.now && me.valid_password?(params[:user][:password]) != me.valid_password?('password')
+      logger.debug "=========Logging in , based on role========"
+      session[:user] = me.id
+      if me.has_role? "Admin"
+        dashboard_path
+      else
+        store_selections_index_path
+      end
+    end
+  end
 
 
- def after_sign_in_path_for(user)
-   logger.debug "========after signin==========="
- 	if me.valid_duration >= Time.now && me.valid_password?(params[:user][:password]) == me.valid_password?('password')
-
-           #store current user in session
-           session[:user] = me.id
-			     password_edit_user_path(me.id)
-	elsif me.valid_duration < Time.now
-		       sign_out(@user)
-		       #redirect_to destroy_user_session_path, flash[:notice]= 'Your Validaty Period Has Expired!,Speak to Admin'
-	else  me.valid_duration >= Time.now  &&  me.valid_password?(params[:user][:password]) != me.valid_password?('password')
-		       if me.has_role? :admin
-		           dashboard_path
-		      else
-		           store_selections_index_path
-		     end
-		end
-	end
-
+  #def after_sign_out_path_for(user)
+  #  new_user_session_path
+  #end
 
 
 end

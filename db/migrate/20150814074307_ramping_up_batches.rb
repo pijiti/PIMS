0817d66c5e13631d024421b68e00a676a077ec74
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-08-29 12:37:32 +0530

    safety check in migration file

--- db/migrate/20150814074307_ramping_up_batches.rb
+++ db/migrate/20150814074307_ramping_up_batches.rb
@@ -2,7 +2,7 @@ class RampingUpBatches < ActiveRecord::Migration
 
   def change
      Batch.all.each do |batch|
-      next if !batch.inventory_batches.blank?
+      next if !batch.inventory_batches.blank?  or batch.supply.blank?
       InventoryBatch.create(:inventory => Inventory.where(:brand_id => batch.brand_id , :store => batch.supply.store).first ,
                             :batch => batch, :units =>  batch.qty.to_i * batch.brand.pack_size.to_i)
     end

c3c3c2a25dda18ddc2c15953a6e1a955947d7dd0
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-09-28 18:17:45 +0530

    added validation for min dispensable in backend

--- app/models/prescription_batch.rb
+++ app/models/prescription_batch.rb
@@ -3,9 +3,12 @@ class PrescriptionBatch < ActiveRecord::Base
   belongs_to :pharm_item
   belongs_to :brand
   belongs_to :store
-  validates_presence_of :qty, :rate , :store_id
+  validates :qty, presence: true , numericality:{greater_than: 0}
+  validates_presence_of :rate , :store_id
   validate :rate_blank_check
   validate :check_available_qty
+  validate :multiples_of_min_dispensable
+
   before_update :total_calculation
 
   def rate_blank_check
@@ -30,6 +33,12 @@ class PrescriptionBatch < ActiveRecord::Base
       errors.add(:brand , "#{brand.try(:name)} is unavailable in the store")
     elsif available_units.to_i < self.qty.to_i
       errors.add("" , "#{available_units} of #{brand.try(:name)} available in the store")
+    end
+  end
+
+  def multiples_of_min_dispensable
+    if qty.to_i % brand.min_dispensable != 0
+      errors.add("" , "#{brand.name} must be dispensed as multiples of #{brand.min_dispensable}" )
     end
   end
 

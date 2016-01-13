a794a13ff968737c0124609f36d53ae2684e79ae
Ganesh Maduranathan <gmaduranathan@anpi.com>
2016-01-13 23:27:01 +0530

    added stacked column highcharts

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -3,49 +3,74 @@
  */
 $(function () {
     $('#drugs_chart_container').highcharts({
-        title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
+        chart: {
+            type: 'column'
         },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
+        title: {
+            text: 'Users vs Stores'
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            categories: ['A&E Ward', 'Afternoon Dispensary', 'Ante Natal Clinic', 'Chest Clinic', 'Copd Injection Room']
         },
         yAxis: {
+            min: 0,
+            allowDecimals: false,
             title: {
-                text: 'Temperature (°C)'
+                text: 'Count'
             },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '°C'
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
         },
         legend: {
-            layout: 'vertical',
             align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        credits: {
+            enabled: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }
         },
         series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            name: 'Store Keeper',
+            data: [5, 3, 4, 7, 2]
         }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            name: 'Store Manager',
+            data: [2, 2, 3, 2, 1]
         }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            name: 'Pharmacist',
+            data: [3, 4, 4, 2, 5]
         }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            name: 'Pharmacy Technician',
+            data: [3, 1, 2, 4, 5]
         }]
     });
 });

f11ce793d741bf1e1579c201584d56afa56781a1
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2016-01-15 20:44:35 +0530

    generic drugs with brands count graph

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,18 @@ class ReportsController < ApplicationController
 
   def drugs
     @item_classes = ItemClass.includes(:sub_classes).all
+    @chart_data = []
+    @item_classes.each do |item|
+      @chart_data << {:name => item.sub_classes.pluck(:name).map { |x| x.gsub("'", "") }, :y => item.sub_classes.try(:count), :item => item.name.gsub("'", "")}
+    end
+
+    @pharm_items = PharmItem.includes(:brands).all
+
+    @brand_data = []
+    @pharm_items.each do |p|
+      @brand_data << {:name => p.brands.pluck(:name).map { |x| x.gsub("'", "") }, :y => p.brands.try(:count), :drug => p.name.gsub("'", "")}
+    end
+
   end
 
   def stores

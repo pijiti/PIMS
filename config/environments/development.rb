062df36ea9693ce25f93211ec527e32c6ed824de
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2016-02-03 13:14:37 +0530

    logging levels changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-  config.assets.logger = true
+  config.assets.logger = false
+  config.log_level = :info
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
